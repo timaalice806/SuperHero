eedeee554cc3fc756c2ef7fa4aa192010b9e7342
fatima <tima.alice806@gmail.com>
2020-02-17 07:47:41 -0600

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Super_Hero.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/SuperHero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Super_Hero.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string AlterEgoName { get; set; }
+        public string PrimaryPower { get; set; }
+        public string SecondaryPower { get; set; }
+        public string CatchPhrase { get; set; }
+    }
+}
